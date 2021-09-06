3f75f1e96f8cf50af5874b10548025391f3c7cb3
Masaya Kameyama <masaya@MasayanoMac-mini.local>
2021-09-06 10:42:54 +0900

    delete old init files

--- inits/20_package.el
+++
@@ -1,9 +0,0 @@
-(require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
-(package-initialize)
-(when (not package-archive-contents) (package-refresh-contents))
-
-(let ((default-directory "~/.emacs.d/elpa/"))
-  (normal-top-level-add-to-load-path '("."))
-  (normal-top-level-add-subdirs-to-load-path))
