e39b36c47b0e79ac1da82b24bbd1bdb043cc1197
Masaya Kameyama <masaya.kameyma@flinters.co.jp>
2024-07-12 15:41:10 +0900

    add

---
+++ var/bookmark-default.el
@@ -0,0 +1,15 @@
+;;;; Emacs Bookmark Format Version 1;;;; -*- coding: utf-8-emacs; mode: lisp-data -*-
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("init.el"
+ (filename . "~/.emacs.d/init.el")
+ (front-context-string . ";; init.el is cr")
+ (rear-context-string)
+ (position . 1))
+("init.org"
+ (filename . "~/.emacs.d/init.org")
+ (front-context-string . "\n\nemacsのinit.elを")
+ (rear-context-string . "STARTUP: fold\n\n\n")
+ (position . 111))
+)
